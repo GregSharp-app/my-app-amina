c4d5e7261a29ee17fa22ca47caad88e720d49eae
rencode <greg.sharp.app@gmail.com>
2024-11-12 16:12:48 +0100

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="bg-red-500">Welcome to ds SvelteKit</h1>
+<h1 class="bg-red-500">TEST ANDROID APP</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
